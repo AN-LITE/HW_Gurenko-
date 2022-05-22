0c4654bf9d7761121f5d03b53de28c63d671e4e0
Andrey Gurenko <89606947+AN-LITE@users.noreply.github.com>
2022-05-22 14:26:33 +0300

    HW_30

---
+++ HW_30 (This)/js/script.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const newCar = {
+
+    name: 'Tesla',
+    model : 'X',
+    year : 2019,
+    price : '95000$',
+
+    infoCar : function (){
+
+        console.group(`${this.name} info`)
+        console.log(`Name is ${this.name}`);
+        console.log(`Model is ${this.model}`);
+        console.log(`Year is ${this.year}`);
+        console.log(`price is ${this.price}`);
+        console.groupEnd();
+
+    }   
+}
+
+
+const myCar = {
+
+    name: 'Toyota',
+    model : 'Corolla',
+    year : 2008,
+    price : '9500$',
+
+}
+
+
+newCar.infoCar();
+newCar.infoCar.bind(myCar)();
