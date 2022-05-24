27d1e9fa767d230501a3c664c7b0d5ec8f4ab197
Andrey Gurenko <89606947+AN-LITE@users.noreply.github.com>
2022-05-24 20:50:38 +0300

    HW_30

--- HW_30 (This)/js/script.js
+++ HW_30 (This)/js/script.js
@@ -1,24 +1,12 @@
 'use strict'
 
-const newCar = {
+function bindRef(ctx,func){
 
-    name: 'Tesla',
-    model : 'X',
-    year : 2019,
-    price : '95000$',
+    return function (){
+        func.call(ctx)
+    }
 
-    infoCar : function (){
-
-        console.group(`${this.name} info`)
-        console.log(`Name is ${this.name}`);
-        console.log(`Model is ${this.model}`);
-        console.log(`Year is ${this.year}`);
-        console.log(`price is ${this.price}`);
-        console.groupEnd();
-
-    }   
 }
-
 
 const myCar = {
 
@@ -29,6 +17,48 @@ const myCar = {
 
 }
 
+function src (){
 
-newCar.infoCar();
-newCar.infoCar.bind(myCar)();
+    console.log(this);
+}
+
+let pack = bindRef(myCar,src)
+pack();
+
+
+
+
+
+
+// const newCar = {
+
+//     name: 'Tesla',
+//     model : 'X',
+//     year : 2019,
+//     price : '95000$',
+
+//     infoCar : function (){
+
+//         console.group(`${this.name} info`)
+//         console.log(`Name is ${this.name}`);
+//         console.log(`Model is ${this.model}`);
+//         console.log(`Year is ${this.year}`);
+//         console.log(`price is ${this.price}`);
+//         console.groupEnd();
+
+//     }   
+// }
+
+
+// const myCar = {
+
+//     name: 'Toyota',
+//     model : 'Corolla',
+//     year : 2008,
+//     price : '9500$',
+
+// }
+
+
+// newCar.infoCar();
+// newCar.infoCar.bind(myCar)();
